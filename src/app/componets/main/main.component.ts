cd095e29feca90c36440306d0a0bbf430f7cafb8
bensammwaniki <bensammwaniki@gmail.com>
2021-08-22 18:28:02 +0300

    able to loop through data

--- src/app/componets/main/main.component.ts
+++ src/app/componets/main/main.component.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/quote';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
+  templateUrl:'./main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  quoter = new Quote(0,"","","","",new Date(),0,0);
-  @Output() add = new EventEmitter<quote>();
 
-  addQuote(){
-this.add.emit(this.quoter);
-this.quoter = new Quote(0,"","","","",new Date(),0,0);
-  }
+ constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
